4da86b369e8bc013b7879043a64039cccfa11345
Sahil Kumar <sahilkumar.sk148@gmail.com>
2025-02-01 15:58:57 +0530

    Update README.md

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# React + Vite
+# ZeroBin
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- Developed a smart waste management platform connecting users with recyclers to promote sustainability.
+- Integrated location-based pickups, real-time tracking, and an eco-reward system to incentivize responsible waste disposal.
+- Built an AI-powered Chat Assistant (Gemini API) for text and image-based waste guidance.
+- Added a waste segregation game for user education.
+- Implemented Shiprocket API for logistics and Google Maps Places API to locate recyclers within 20km.
+- Used Supabase for authentication and storage to ensure secure user data handling.
+- Finalist among 15 teams (out of 145) in Hackformers Hackathon, recognized for sustainability innovation
--- helpers/supabase.js
+++ helpers/supabase.js
@@ -1,11 +1,15 @@
-import { useState, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
 
-const supabaseUrl = "https://olmdgcdchqbpgmkkmvnk.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sbWRnY2RjaHFicGdta2ttdm5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxNzY4NTgsImV4cCI6MjA1Mzc1Mjg1OH0.njL_mjOsx7krTp-h1rnL0C9rHxvFVchYyaVzHS4fTHc";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 export default supabase;
+
+// const supabaseUrl = "https://olmdgcdchqbpgmkkmvnk.supabase.co";
+// const supabaseAnonKey =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sbWRnY2RjaHFicGdta2ttdm5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxNzY4NTgsImV4cCI6MjA1Mzc1Mjg1OH0.njL_mjOsx7krTp-h1rnL0C9rHxvFVchYyaVzHS4fTHc";
+
+// const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// export default supabase;
